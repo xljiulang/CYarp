c6d2e5486e8a16ff524014f3eae810d9e94c1254
xljiulang <366193849@qq.com>
2024-03-24 19:06:29 +0800

    完善注释和说明

--- CYarp.Server/Configs/AuthorizationConfig.cs
+++ CYarp.Server/Configs/AuthorizationConfig.cs
@@ -9,11 +9,12 @@ namespace CYarp.Server.Configs
     {
         /// <summary>
         /// 允许的角色
+        /// 角色的CiaimType为http://schemas.microsoft.com/ws/2008/06/identity/claims/role
         /// </summary>
         public string[] AllowRoles { get; set; } = Array.Empty<string>();
 
         /// <summary>
-        /// ClientId的ClaimType，默认为http://schemas.xmlsoap.org/ws/2005/05/identity/claims/sid
+        /// IClient的Id对应的ClaimType，默认为http://schemas.xmlsoap.org/ws/2005/05/identity/claims/sid
         /// </summary>
         public string ClientIdClaimType { get; set; } = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/sid";
     }
