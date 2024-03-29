02a4ffca7d53e2007f8e06654d2f9846236d5b5a
陈国伟 <366193849@qq.com>
2024-03-29 11:02:19 +0800

    增加AuthenticationSchemes的配置

--- CYarp.Server/Configs/AuthorizationConfig.cs
+++ CYarp.Server/Configs/AuthorizationConfig.cs
@@ -11,6 +11,11 @@ namespace CYarp.Server.Configs
         private AuthorizationPolicy? policy;
 
         /// <summary>
+        /// 获取或设置身份验证方案
+        /// </summary>
+        public string[] AuthenticationSchemes { get; set; } = Array.Empty<string>();
+
+        /// <summary>
         /// 允许的角色
         /// 无角色表示不验证角色
         /// 角色的CiaimType为http://schemas.microsoft.com/ws/2008/06/identity/claims/role
@@ -20,7 +25,7 @@ namespace CYarp.Server.Configs
         /// <summary>
         /// IClient的Id对应的ClaimType
         /// 默认值为http://schemas.xmlsoap.org/ws/2005/05/identity/claims/sid
-        /// 默认的IClientIdProvider在实现上依赖此属性
+        /// 默认的IClientIdProvider在实现上使用此属性
         /// </summary>
         public string ClientIdClaimType { get; set; } = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/sid";
 
@@ -34,8 +39,9 @@ namespace CYarp.Server.Configs
             if (this.policy == null)
             {
                 var builder = new AuthorizationPolicyBuilder()
-                    .RequireAuthenticatedUser()
+                    .AddAuthenticationSchemes(this.AuthenticationSchemes)
                     .RequireClaim(this.ClientIdClaimType);
+
                 if (this.AllowRoles.Length > 0)
                 {
                     builder.RequireRole(this.AllowRoles);
