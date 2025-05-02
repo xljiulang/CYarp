e81c55a88ffd2b9c85513dffbfbd690d57e9f870
wuchang <wuchangx@qq.com>
2025-05-02 09:30:51 +0800

    增加 sample proj Cyarp.Sample.IntranetClient ，winform 的内网代理网关，把公网的请求转发到内网多个网站。

---
+++ samples/aspnet-sites-tunnel/Cyarp.Sample.IntranetClient/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Cyarp.Sample.IntranetClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            txtServerUri = new TextBox();
+            dataGridView1 = new DataGridView();
+            bindingSource1 = new BindingSource(components);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(41, 44);
+            label1.Name = "label1";
+            label1.Size = new Size(65, 17);
+            label1.TabIndex = 0;
+            label1.Text = "ServerUri:";
+            // 
+            // txtServerUri
+            // 
+            txtServerUri.Location = new Point(112, 38);
+            txtServerUri.Name = "txtServerUri";
+            txtServerUri.Size = new Size(305, 23);
+            txtServerUri.TabIndex = 2;
+            txtServerUri.Text = "http://localhost:5080";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(41, 97);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.CellSelect;
+            dataGridView1.Size = new Size(691, 260);
+            dataGridView1.TabIndex = 3;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(775, 395);
+            Controls.Add(dataGridView1);
+            Controls.Add(txtServerUri);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtServerUri;
+        private DataGridView dataGridView1;
+        private BindingSource bindingSource1;
+    }
+}
---
+++ samples/aspnet-sites-tunnel/Cyarp.Sample.IntranetClient/ProxySiteConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cyarp.Sample.IntranetClient;
+
+
+public delegate void ProxySiteConfigEnableChangedEventHandler(ProxySiteConfig e);
+
+public class ProxySiteConfig
+{
+    public event ProxySiteConfigEnableChangedEventHandler? OnEnabledChange;
+
+    public bool Enable
+    {
+        get;
+        set
+        {
+            field = value;
+            OnEnabledChange?.Invoke(this);
+        }
+    }
+
+    public string? Domain { get; set; }
+
+    public string? TargetUri { get; set; }
+
+    public CancellationTokenSource WorkerCancelTokenSource { get; set; }
+
+    public ProxySiteConfig()
+    {
+        WorkerCancelTokenSource = new CancellationTokenSource();
+    }
+
+
+}
+
